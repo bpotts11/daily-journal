ec5937ed7795f2948dff782eb62e91be960c7236
bpotts11 <brittneympotts@gmail.com>
2021-02-10 00:12:20 -0600

    created and api to store journal entries in and fetched them to populate that saved journal entries aside

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,44 +1,17 @@
-/*
- *   Journal data provider for Daily Journal application
- *
- *      Holds the raw data about each entry and exports
- *      functions that other modules can use to filter
- *      the entries for different purposes.
- */
+let journal = [
 
-// This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-    {
-        id: 2,
-        date: "07/25/2025",
-        concept: "Flexbox",
-        entry: "We talked about Flexbox components. I was able to use it to create two sections side by side.",
-        mood: "Happy"
-    },
-    {
-        id: 3,
-        date: "07/26/2025",
-        concept: "JavaScript",
-        entry: "We talked about JavaScript. It is a lot to take in. I need to spend more time learning functions",
-        mood: "sad"
-    }
 ]
 
-/*
-    You export a function that provides a version of the
-    raw data in the format that you want
-*/
-export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
-        (currentEntry, nextEntry) =>
-            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-    )
-    return sortedByDate
+export const getEntries = () => {
+    return fetch("http://localhost:8088/entries") // Fetch from the API
+        .then(response => response.json())  // Parse as JSON
+        .then(entries => {
+            journal = entries
+            // console.log(journal)
+            // What should happen when we finally have the array?
+        })
+}
+
+export const useEntries = () => {
+    return journal.slice()
 }
--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -4,27 +4,29 @@
  *    there are items in the collection exposed by the
  *    data provider component
  */
-import { useJournalEntries } from "./JournalDataProvider.js"
+import { getEntries, useEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
 // DOM reference to where all entries will be rendered
 const entryLog = document.querySelector("#entryLog")
 
-export const EntryListComponent = () => {
-    // Use the journal entry data from the data provider component
-    const entries = useJournalEntries()
+// I am importing from journal data provider to get the entries from the api so I can map them and turn them into strings so they can be impoted into the HTML rep from journal entry
+const render = (entryArray) => {
+    const entriesConvertedToString = entryArray.map(entryObj => {
+        return JournalEntryComponent(entryObj)
+    }).join("")
 
-    let entryHTMLRepresentation = ""
-    for (const entry of entries) {
-        /*
-          Invoke the component that returns an
-          HTML representation of a single entry
-       */
-        entryHTMLRepresentation += JournalEntryComponent(entry)
-    }
     entryLog.innerHTML += `
-    <div>
-        ${entryHTMLRepresentation}
+    <div class="savedEntryContainer">
+        ${entriesConvertedToString}
     </div>
     `
+}
+
+export const entyList = () => {
+    getEntries()
+        .then(() => {
+            const allEntries = useEntries()
+            render(allEntries)
+        })
 }
